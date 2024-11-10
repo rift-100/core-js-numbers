02dcaf097bd4c722fc4bd76702b1bc368ee3c29f
Sergei Shinkarenko <exp33int@gmail.com>
2024-11-10 11:30:03 +0500

    feat: implement toNumber func

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -223,8 +223,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const convertedValue = Number(value);
+  if (Number.isNaN(convertedValue)) {
+    return def;
+  }
+  return convertedValue;
 }
 
 /**
