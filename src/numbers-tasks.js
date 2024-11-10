58ab8c6e21cbbdcf241891397f22d2c799ce0a36
Sergei Shinkarenko <exp33int@gmail.com>
2024-11-10 11:45:47 +0500

    feat: implement getFibonacciNumber func

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -259,8 +259,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) {
+    return 0;
+  }
+  let first = 0;
+  let second = 1;
+  for (let i = 2; i < index; i += 1) {
+    const temp = second;
+    second += first;
+    first = temp;
+  }
+  return first + second;
 }
 
 /**
